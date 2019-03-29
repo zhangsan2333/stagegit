5b218ffede30e177e28826ff7bd70eea280cfc29
testglb <testglb@connext.com>
2019-03-29 15:17:26 +0800

    hotfix222222

--- src/main/java/com/connext/git/BBBBBB.java
+++ src/main/java/com/connext/git/BBBBBB.java
@@ -6,6 +6,7 @@ public class BBBBBB {
    public static void main(String[] args) {
 	System.out.println("stage2222222。。。");
 	System.out.println("stage11111。。。");
+	System.out.println("hot_fix...");
 }
 	
 	
